d59503ad1d09b749d6198c4e65973a7e32ebd6fd
moreliaoscori <130956218+moreliaoscori@users.noreply.github.com>
2023-04-17 01:02:37 -0400

    Add files via upload

---
+++ pregunta1ex/sobrenosotros.php
@@ -0,0 +1,134 @@
+<?php include 'cabecera.php';?>
+<?php include 'menu.php';?>
+<html>
+  <!-- client section -->
+  <section class="client_section layout_padding">
+    <h2 class="">
+      Testimonial
+    </h2>
+    <div class="container">
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+              <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+              <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                  <div class="row">
+                      <div class="col-md-3">
+                        <div class="client_img-box">
+                          <img src="images/client.png" alt="">
+                        </div>
+                      </div>
+                      <div class="col-md-9">
+                        <div class="client_detail-box">
+                          <h5>
+                            Alex Du
+                          </h5>
+                          <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+                            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+              </div>
+              <div class="carousel-item">
+                  <div class="row">
+                      <div class="col-md-3">
+                        <div class="client_img-box">
+                          <img src="images/client.png" alt="">
+                        </div>
+                      </div>
+                      <div class="col-md-9">
+                        <div class="client_detail-box">
+                          <h5>
+                            Alex Du
+                          </h5>
+                          <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+                            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+              </div>
+              <div class="carousel-item">
+                  <div class="row">
+                      <div class="col-md-3">
+                        <div class="client_img-box">
+                          <img src="images/client.png" alt="">
+                        </div>
+                      </div>
+                      <div class="col-md-9">
+                        <div class="client_detail-box">
+                          <h5>
+                            Alex Du
+                          </h5>
+                          <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                            dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+                            ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+              </div>
+            </div>
+          </div>
+     
+    </div>
+  </section>
+  
+  <section class="contact_section ">
+
+<div class="container">
+ 
+  <div class="row">
+    <div class="col-md-6">
+      <div class="d-flex justify-content-center d-md-block">
+        <h2>
+          Contact Us
+        </h2>
+      </div>
+      <form action="">
+        <div class="contact_form-container">
+          <div>
+            <div>
+              <input type="text" placeholder="Name">
+            </div>
+            <div>
+              <input type="email" placeholder="Email">
+            </div>
+            <div>
+              <input type="text" placeholder="Phone Number">
+            </div>
+            <div class="mt-5">
+              <input type="text" placeholder="Message">
+            </div>
+            <div class="mt-5">
+              <button type="submit">
+                send
+              </button>
+            </div>
+          </div>
+
+        </div>
+
+      </form>
+    </div>
+    <div class="col-md-6">
+      <div class="contact_img-box">
+        <img src="images/students.jpg" alt="">
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+<!-- end contact section -->
+
+  </html>
+<?php include 'pie.php';?>
