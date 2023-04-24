ae6c683508aad7bf31c2f211f9ba6c48094782b5
moreliaoscori <130956218+moreliaoscori@users.noreply.github.com>
2023-04-24 02:45:33 -0400

    Add files via upload

---
+++ pregunta1ex/fisica.php
@@ -0,0 +1,33 @@
+<?php include 'cabecera.php';?>
+<?php include 'menu.php';?>
+<html> 
+<section class="why_section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <div class="why_img-container">
+              <img src="images/why.png" alt="">
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="why_detail-box">
+              <h3>
+                FISICA
+              </h3>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
+                ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+              </p>
+              <div class="">
+                <a href="" class="call_to-btn btn_white-border">
+                  Read More
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+</html>
+<?php include 'pie.php';?>
