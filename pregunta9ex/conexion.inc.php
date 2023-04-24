802ec855fcf58e17a087ddf7f6d0f5ac88da9b83
moreliaoscori <130956218+moreliaoscori@users.noreply.github.com>
2023-04-24 02:47:43 -0400

    Delete pregunta9ex directory

--- pregunta9ex/conexion.inc.php
+++
@@ -1,4 +0,0 @@
-<?php
-$con=mysqli_connect("localhost","more","5498");
-mysqli_select_db($con, "mbdmoreliaoscori");
-?>
--- pregunta9ex/eliminar.php
+++
@@ -1,9 +0,0 @@
-<?php
-include "conexion.inc.php";
-$id=$_GET["ci"];
-$sql="delete from persona where ci=$id";
-mysqli_query($con, $sql);
-?>
-<img height="30" width="30" src="./imagenes/eliminar.jpg"/>
-<br/>
-<a href="inicial.php">Volver</a>
